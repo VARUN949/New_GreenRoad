5d903066bf43116752e60177e929ade7436e1982
Varun Thummar <varunthummar@192.168.1.3>
2024-03-24 16:49:02 +0530

    small changes

--- src/Components/All_Circles/CirclesList.jsx
+++ src/Components/All_Circles/CirclesList.jsx
@@ -32,6 +32,9 @@ export default function CirclesList() {
                         )
                     })
                 }
+                {
+                    Object.keys(circleList.circles).length > 3 && activeCircles()
+                }
             </div>
         </div>
     )
