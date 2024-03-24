2cc405cf75eeb6cf318d9408ea47b7164b64a469
Varun Thummar <varunthummar@192.168.1.3>
2024-03-24 09:05:08 +0530

    modified

--- src/Components/All_Circles/CirclesList.jsx
+++ src/Components/All_Circles/CirclesList.jsx
@@ -3,40 +3,34 @@ import Circle from './Circle'
 
 export default function CirclesList() {
 
-    const [circleList, setCircleList] = useState({ circle: {} });
+    const [circleList, setCircleList] = useState({ circles: {} });
 
     const activeCircles = () => {
         document.getElementById("circlelist").style.height = "auto"
     }
 
     useEffect(() => {
-
         const AllCircle = async () => {
-            const response = await fetch('https://greenroad-gr.onrender.com/app/p1/get-circle', {
+
+            const response = await fetch(`https://greenroad-gr.onrender.com/app/p1/get-circle`, {
                 method: 'GET',
             });
             const data = await response.json()
             setCircleList(data)
-
         }
         AllCircle()
-        // eslint - disable - next - line react - hooks / exhaustive - deps
+        // eslint - disable - next - li / ne react - hooks / exhaustive - deps
     }, [])
 
     return (
         <div className="bg-slate-700 h-[94vh] flex justify-center" id="circlelist">
             <div className='w-1/3 h-58  h-auto flex-col mt-28'>
-
-
-                {Object.keys(circleList.circle).length > 0 &&
-                    circleList.circle.map((circle, index) => {
+                {Object.keys(circleList.circles).length > 0 &&
+                    circleList.circles.map((circle, index) => {
                         return (
                             <Circle circle={circle} key={index} />
                         )
                     })
-                }
-                {
-                    Object.keys(circleList.circle).length > 3 && activeCircles()
                 }
             </div>
         </div>
